b2426e3eee48c7e445b8539f2982808285a0665d
Matheus Ferreira Geres <matheus.geres@keyrus.com.br>
2019-08-08 11:18:50 -0300

    Melhoria para perguntar qual período para lançar horas da planilha.

--- src/index.js
+++ src/index.js
@@ -5,9 +5,21 @@ const chrome = require('./robot/chrome');
 (async () => {
   let month = "AGO";
   let fileName = "meuponto_2019.xlsx";
+
+  let periodRead = parseInt(readline.question(
+    `Do you want read entire timesheet, current week or last week? \nUse one of these options:
+    1 = Entire Timesheet
+    2 = Current week
+    3 = Last week\n`));
+
+  let periodsToReadValid = [1,2,3];
+  if(periodsToReadValid.indexOf(periodRead)<0){
+    console.log("You enter not valid period. Try again, please! :D");
+    return;
+  }
   
   let daysToInput = await excel.readTimetableFromExcel(fileName, month);
-  console.log(daysToInput);
+  console.table(daysToInput);
 
   let doInput = readline.question("Do you want continue with input of hours? [y/N]");
   if(doInput.toLowerCase()=="y"){
